c4f51c3d63da1695347cf27dc34804d7322c497b
Felipe Parra <fpr29.10@gmail.com>
2023-04-24 13:18:44 -0600

    Update

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseAxios = axios.create({
-  baseURL: "http://0.0.0.0:8081",
+  baseURL: "http://localhost:8081",
   headers: {
     "Content-Type": "application/json",
   },
